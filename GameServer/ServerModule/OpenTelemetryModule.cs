5b668250e442fc890af8257ea096a5c10c481f2b
BonOeil <fabrice.debonlier@gmail.com>
2025-06-18 15:43:13 +0200

    Delete extension class

--- GameServer/ServerModule/OpenTelemetryModule.cs
+++ GameServer/ServerModule/OpenTelemetryModule.cs
@@ -4,6 +4,11 @@
 
 namespace GameServer.ServerModule
 {
+    using OpenTelemetry;
+    using OpenTelemetry.Exporter;
+    using OpenTelemetry.Metrics;
+    using OpenTelemetry.Trace;
+
     public class OpenTelemetryModule : IServerModule
     {
         public void PostBuild(WebApplication app)
@@ -13,7 +18,60 @@ namespace GameServer.ServerModule
 
         public void PreBuild(WebApplicationBuilder builder)
         {
-            builder.ConfigureOpenTelemetry();
+            ConfigureOpenTelemetry(builder);
+        }
+
+        public IHostApplicationBuilder ConfigureOpenTelemetry(IHostApplicationBuilder builder)
+        {
+            builder.Services.Configure<OtlpExporterOptions>(
+                    o => o.Headers = $"x-otlp-api-key={Environment.GetEnvironmentVariable("DASHBOARD__OTLP__AUTHMODE")}");
+
+            builder.Logging.AddOpenTelemetry(logging =>
+            {
+                logging.IncludeFormattedMessage = true;
+                logging.IncludeScopes = true;
+            });
+
+            builder.Services.AddOpenTelemetry()
+                .WithMetrics(metrics =>
+                {
+                    metrics.AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddRuntimeInstrumentation();
+                })
+                .WithTracing(tracing =>
+                {
+                    tracing.AddAspNetCoreInstrumentation()
+
+                        // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
+                        // .AddGrpcClientInstrumentation()
+                        .AddHttpClientInstrumentation();
+                })
+                /*.WithLogging(logging => logging
+                .AddOtlpExporter())*/;
+
+            AddOpenTelemetryExporters(builder);
+
+            return builder;
+        }
+
+        private IHostApplicationBuilder AddOpenTelemetryExporters(IHostApplicationBuilder builder)
+        {
+            var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+
+            if (useOtlpExporter)
+            {
+                builder.Services.AddOpenTelemetry().UseOtlpExporter();
+            }
+
+            // Uncomment the following lines to enable the Azure Monitor exporter (requires the Azure.Monitor.OpenTelemetry.AspNetCore package)
+            // if (!string.IsNullOrEmpty(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
+            // {
+            //    builder.Services.AddOpenTelemetry()
+            //       .UseAzureMonitor();
+            // }
+
+            return builder;
         }
     }
 }
