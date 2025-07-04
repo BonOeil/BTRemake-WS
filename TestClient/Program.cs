e2c21387f696d6b572da0315456faabf728f50d6
BonOeil <fabrice.debonlier@gmail.com>
2025-07-04 16:50:03 +0200

    top level statements

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -2,53 +2,60 @@
 // Copyright (c) BTRemake. All rights reserved.
 // </copyright>
 
-// See https://aka.ms/new-console-template for more information
-#pragma warning disable SA1200 // Using directives should be placed correctly
-using GameShared.Game;
-using GameShared.Game.Entities;
-using GameShared.Messages;
-using Microsoft.AspNetCore.SignalR.Client;
-using Microsoft.Extensions.Configuration;
-using MongoDB.Driver;
-#pragma warning restore SA1200 // Using directives should be placed correctly
-
-Console.WriteLine("Hello, World!");
-
-var configuration = new ConfigurationBuilder()
-            .AddUserSecrets<Program>().Build();
-var mongoConnectionString = configuration["ConnectionStrings:MongoDb"];
-
-var client = new MongoClient(mongoConnectionString);
-
-// Clear DB ? or create new DB as gameName
-var dataBase = client.GetDatabase("gameName");
-
-// Read locations
-
-// Read OoB
-
-// Save into DB
-// await dataBase.CreateCollectionAsync(nameof(Location));
-var collection = dataBase.GetCollection<Location>(nameof(Location));
-Location location = new Location("London", new GPSPosition(51.5074f, -0.1278f), Faction.Allies);
-await collection.InsertOneAsync(location);
-location = new Location("Berlin", new GPSPosition(52.5200f, 13.4050f), Faction.Axis);
-await collection.InsertOneAsync(location);
-// await AddLocation("London", new GPSPosition(51.5074f, -0.1278f), Faction.Allies);
-// await AddLocation("Berlin", new GPSPosition(52.5200f, 13.4050f), Faction.Axis);
-
-var serverUrl = Environment.GetEnvironmentVariable("API_URL") ?? "http://localhost:8080";
-
-// var serverUrl = "https://localhost:32790/GameHub";
-var connection = new HubConnectionBuilder()
-            .WithUrl($"{serverUrl}/GameHub")
-            .WithAutomaticReconnect()
-            .Build();
-
-await connection.StartAsync();
-
-await connection.InvokeAsync(nameof(LoadScenario), new LoadScenario
+namespace GameServer
 {
-    InstanceName = "test",
-    ScenarioName = "test",
-});
+    // See https://aka.ms/new-console-template for more information
+    using GameShared.Game;
+    using GameShared.Game.Entities;
+    using GameShared.Messages;
+    using Microsoft.AspNetCore.SignalR.Client;
+    using Microsoft.Extensions.Configuration;
+    using MongoDB.Driver;
+
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                        .AddUserSecrets<Program>(false)
+                        .Build();
+
+            var mongoConnectionString = configuration.GetConnectionString("MongoDb");
+
+            var client = new MongoClient(mongoConnectionString);
+
+            // Clear DB ? or create new DB as gameName
+            var dataBase = client.GetDatabase("gameName");
+
+            // Read locations
+
+            // Read OoB
+
+            // Save into DB
+            // await dataBase.CreateCollectionAsync(nameof(Location));
+            var collection = dataBase.GetCollection<Location>(nameof(Location));
+            Location location = new Location("London", new GPSPosition(51.5074f, -0.1278f), Faction.Allies);
+            await collection.InsertOneAsync(location);
+            location = new Location("Berlin", new GPSPosition(52.5200f, 13.4050f), Faction.Axis);
+            await collection.InsertOneAsync(location);
+            // await AddLocation("London", new GPSPosition(51.5074f, -0.1278f), Faction.Allies);
+            // await AddLocation("Berlin", new GPSPosition(52.5200f, 13.4050f), Faction.Axis);
+
+            var serverUrl = Environment.GetEnvironmentVariable("API_URL") ?? "http://localhost:8080";
+
+            // var serverUrl = "https://localhost:32769/GameHub";
+            var connection = new HubConnectionBuilder()
+                        .WithUrl($"{serverUrl}/GameHub")
+                        .WithAutomaticReconnect()
+                        .Build();
+
+            await connection.StartAsync();
+
+            await connection.InvokeAsync(nameof(LoadScenario), new LoadScenario
+            {
+                InstanceName = "test",
+                ScenarioName = "test",
+            });
+        }
+    }
+}
