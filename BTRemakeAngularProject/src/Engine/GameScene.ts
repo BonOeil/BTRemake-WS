7d5f14dcf789401dc85d57974921aa1311b69c60
BonOeil <fabrice.debonlier@gmail.com>
2025-07-07 09:56:49 +0200

    Create dedicated scene class

---
+++ BTRemakeAngularProject/src/Engine/GameScene.ts
@@ -0,0 +1,134 @@
+import { inject } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three-orbitcontrols-ts';
+import { CoordinateConverter } from '../Business/CoordinateConverter';
+import { MapLocation } from '../Models/MapLocation';
+import { LocationService } from '../Services/LocationService';
+import { Subject } from 'rxjs';
+
+export class GameScene {
+
+  scene!: THREE.Scene;
+
+  earth!: THREE.Mesh;
+  controls!: OrbitControls;
+  renderer!: THREE.WebGLRenderer;
+  camera!: THREE.PerspectiveCamera;
+
+  private selectedObjectSubject = new Subject<object | null>();
+  public selectedObject$ = this.selectedObjectSubject.asObservable();
+
+  private selectedObject: THREE.Object3D | null = null;
+  private originalMaterial: THREE.Material | null = null;
+
+  private locationService: LocationService;
+
+  constructor(renderer: THREE.WebGLRenderer, locationService : LocationService) {
+    this.renderer = renderer;
+
+    this.locationService = locationService;
+
+    this.initScene();
+  }
+
+  initScene() {
+    // Scene
+    this.scene = new THREE.Scene();
+    this.scene.add(new THREE.AxesHelper(5));
+    this.earth = this.createPlanet();
+
+    // Camera
+    const renderSize = new THREE.Vector2();
+    this.renderer.getSize(renderSize);
+    this.camera = this.createCamera(renderSize.width / renderSize.height);
+
+    // Lights
+    this.setupLighting();
+
+    // Fill scene
+    this.createLocations();
+
+    // Controls
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.update();
+
+    // Render loop
+    // TODO Out
+    this.renderer.setAnimationLoop(() => this.animate());
+  }
+
+  createCamera(renderRatio: number): THREE.PerspectiveCamera {
+    const camera = new THREE.PerspectiveCamera(75, renderRatio, 0.1, 1000);
+    camera.position.z = CoordinateConverter.EarthRadius + CoordinateConverter.EarthRadius * 0.5;
+
+    return camera;
+  }
+
+  createLocations() {
+    const material = new THREE.MeshBasicMaterial({ color: "#FF0000" });
+
+    const locations = this.locationService.getLocations();
+    locations.subscribe((value: MapLocation[]) => {
+      value.forEach((location: MapLocation) => {
+        const geometry = new THREE.SphereGeometry(10);
+        const position = CoordinateConverter.GpsToWorldPosition(location.position);
+
+        geometry.translate(position.x, position.y, position.z);
+        const locationVue = new THREE.Mesh(geometry, material);
+
+        this.earth.add(locationVue);
+      });
+    });
+  }
+
+  createPlanet(): THREE.Mesh {
+    const geometry = new THREE.SphereGeometry(CoordinateConverter.EarthRadius, 100, 100);
+    const texture = new THREE.TextureLoader().load("assets/textures/earth-diffuse.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    const material = new THREE.MeshPhongMaterial({
+      map: texture
+    });
+
+    const planet = new THREE.Mesh(geometry, material);
+    this.scene.add(planet);
+
+    return planet;
+  }
+
+  setupLighting() {
+    // Ambiant lightning
+    const ambientLight = new THREE.AmbientLight(0x404040, 0.3);
+    this.scene.add(ambientLight);
+
+    // Directionnal light
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
+    directionalLight.position.set(5, 3, 5);
+    directionalLight.castShadow = true;
+
+    // Shadows
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 50;
+    directionalLight.shadow.camera.left = -10;
+    directionalLight.shadow.camera.right = 10;
+    directionalLight.shadow.camera.top = 10;
+    directionalLight.shadow.camera.bottom = -10;
+
+    this.scene.add(directionalLight);
+
+    // Point light
+    const pointLight = new THREE.PointLight(0xffffff, 0.5, 100);
+    pointLight.position.set(-5, -3, -5);
+    this.scene.add(pointLight);
+  }
+
+  animate() {
+    this.earth.rotation.y += 0.001;
+
+    this.controls.update()
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
--- BTRemakeAngularProject/src/app/planet/planet.component.ts
+++ BTRemakeAngularProject/src/app/planet/planet.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import * as THREE from 'three';
-import { CoordinateConverter } from '../../Business/CoordinateConverter';
-import { OrbitControls } from 'three-orbitcontrols-ts';
+import { GameScene } from '../../Engine/GameScene';
 import { LocationService } from '../../Services/LocationService';
-import { GPSPosition } from '../../Models/GPSPosition';
-import { MapLocation } from '../../Models/MapLocation';
 
 @Component({
   selector: 'app-planet',
@@ -13,23 +10,17 @@ import { MapLocation } from '../../Models/MapLocation';
 })
 export class PlanetComponent implements OnInit {
 
-  scene!: THREE.Scene;
+  scene!: GameScene;
   renderHeigth!: number;
   renderWidth!: number;
 
-  planet!: THREE.Mesh;
-  controls!: OrbitControls;
   renderer!: THREE.WebGLRenderer;
-  camera!: THREE.PerspectiveCamera;
 
   @ViewChild('scene_content') scene_content!: HTMLCanvasElement;
 
   private locationService = inject(LocationService);
 
   ngOnInit() {
-
-    this.scene = new THREE.Scene();
-    this.scene.add(new THREE.AxesHelper(5));
 
     this.renderHeigth = 200;
     this.renderWidth = 300;
@@ -38,92 +29,14 @@ export class PlanetComponent implements OnInit {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: scene_content });
     this.renderer.setSize(this.renderWidth, this.renderHeigth);
 
-    this.camera = this.createCamera();
-    this.planet = this.createPlanet();
-
-    this.createLocations();
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.update();
-
-    // Add light
-    this.setupLighting();
+    this.scene = new GameScene(this.renderer, this.locationService);
 
     this.renderer.setAnimationLoop(() => this.animate());
   }
 
-  createLocations() {
-    const material = new THREE.MeshBasicMaterial({ color: "#FF0000" });
-
-    const locations = this.locationService.getLocations();
-    locations.subscribe((value: MapLocation[]) => {
-      value.forEach((location: MapLocation) => {
-        const geometry = new THREE.SphereGeometry(10);
-        const position = CoordinateConverter.GpsToWorldPosition(location.position);
-
-        geometry.translate(position.x, position.y, position.z);
-        const locationVue = new THREE.Mesh(geometry, material);
-
-        this.planet.add(locationVue);
-      });
-    });
-  }
-
-  createCamera(): THREE.PerspectiveCamera {
-    const camera = new THREE.PerspectiveCamera(75, this.renderWidth / this.renderHeigth, 0.1, 1000);
-    camera.position.z = CoordinateConverter.EarthRadius + CoordinateConverter.EarthRadius * 0.5;
-
-    return camera;
-  }
-
-  createPlanet() : THREE.Mesh {
-    const geometry = new THREE.SphereGeometry(CoordinateConverter.EarthRadius, 100, 100);
-    const texture = new THREE.TextureLoader().load("assets/textures/earth-diffuse.jpg");
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    const material = new THREE.MeshPhongMaterial({
-      map: texture
-    });
-
-    const planet = new THREE.Mesh(geometry, material);
-    this.scene.add(planet);
-
-    return planet;
-  }
-
-  setupLighting() {
-    // Ambiant lightning
-    const ambientLight = new THREE.AmbientLight(0x404040, 0.3);
-    this.scene.add(ambientLight);
-
-    // Directionnal light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
-    directionalLight.position.set(5, 3, 5);
-    directionalLight.castShadow = true;
-
-    // Shadows
-    directionalLight.shadow.mapSize.width = 2048;
-    directionalLight.shadow.mapSize.height = 2048;
-    directionalLight.shadow.camera.near = 0.5;
-    directionalLight.shadow.camera.far = 50;
-    directionalLight.shadow.camera.left = -10;
-    directionalLight.shadow.camera.right = 10;
-    directionalLight.shadow.camera.top = 10;
-    directionalLight.shadow.camera.bottom = -10;
-
-    this.scene.add(directionalLight);
-
-    // Point light
-    const pointLight = new THREE.PointLight(0xffffff, 0.5, 100);
-    pointLight.position.set(-5, -3, -5);
-    this.scene.add(pointLight);
-  }
-
   animate() {
-    this.planet.rotation.y += 0.001;
+    this.scene.animate();
 
-    this.controls.update()
-
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.render(this.scene.scene, this.scene.camera);
   }
 }
