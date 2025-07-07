a22c105aae6d7477e426db63b3f7b9f4a37c1569
BonOeil <fabrice.debonlier@gmail.com>
2025-07-07 11:41:26 +0200

    Add load scenario route

--- GameServer/Controllers/GameApiController.cs
+++ GameServer/Controllers/GameApiController.cs
@@ -4,6 +4,8 @@
 
 namespace GameServer.Controllers
 {
+    using GameShared.Messages;
+    using GameShared.Services.Interfaces;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +13,13 @@ namespace GameServer.Controllers
     [ApiController]
     public class GameApiController : ControllerBase
     {
-        public GameApiController(ILogger<GameApiController> logger)
+        public GameApiController(ILogger<GameApiController> logger, IGameServices gameServices)
         {
             Logger = logger;
+            GameServices = gameServices;
         }
+
+        private IGameServices GameServices { get; }
 
         private ILogger<GameApiController> Logger { get; set; }
 
@@ -22,6 +27,20 @@ namespace GameServer.Controllers
         public async Task<ActionResult<string>> Step()
         {
             Logger.LogTrace($"{nameof(Step)}");
+
+            return new ActionResult<string>("Step");
+        }
+
+        [HttpGet("LoadScenario/{scenarioName}")]
+        public async Task<ActionResult<string>> LoadScenario(string scenarioName)
+        {
+            await GameServices.StartScenario(scenarioName, scenarioName);
+
+            // Informer les autres joueurs de la connexion
+            // await Clients.All.SendAsync(nameof(ScenarioLoaded), new ScenarioLoaded());
+
+            // Logger.LogInformation($"Scenario loaded: {scenarioName} ({Context.ConnectionId})");
+            Logger.LogInformation("Scenario loaded: {ScenarioName}", scenarioName);
 
             return new ActionResult<string>("Step");
         }
