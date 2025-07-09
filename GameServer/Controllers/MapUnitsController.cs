4777a2be991e1ebaafdf85106e005320b6f84f69
BonOeil <fabrice.debonlier@gmail.com>
2025-07-09 14:18:07 +0200

    Add map units controls

---
+++ GameServer/Controllers/MapUnitsController.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="MapUnitsController.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameServer.Controllers
+{
+    using GameShared.Game.Mission;
+    using GameShared.Persistence;
+    using Microsoft.Extensions.Logging;
+
+    public class MapUnitsController : CrudController<MapUnit>
+    {
+        public MapUnitsController(ILogger<MapUnitsController> logger, IRepository<MapUnit> repository)
+            : base(logger, repository)
+        {
+        }
+    }
+}
---
+++ GameShared/Game/Mission/MapUnit.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="MapUnit.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameShared.Game.Mission
+{
+    using GameShared.Persistence;
+
+    public class MapUnit : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public GPSPosition Position { get; set; }
+    }
+}
