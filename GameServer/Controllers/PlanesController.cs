518cd8d9379b24d40bbd8ab80157ab1831f85548
BonOeil <fabrice.debonlier@gmail.com>
2025-07-28 08:45:04 +0200

    Add controller

---
+++ GameServer/Controllers/PlanesController.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PlanesController.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameServer.Controllers
+{
+    using GameShared.Game.Entities;
+    using GameShared.Persistence;
+    using Microsoft.Extensions.Logging;
+
+    public class PlanesController : CrudController<Plane>
+    {
+        public PlanesController(ILogger<PlanesController> logger, IRepository<Plane> repository)
+            : base(logger, repository)
+        {
+        }
+    }
+}
---
+++ GameShared/Game/Entities/Plane.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Plane.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameShared.Game.Entities
+{
+    using GameShared.Persistence;
+
+    public class Plane : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
