cb22173bd90bcd2771b4ec4eba64b24f14be51fb
BonOeil <fabrice.debonlier@gmail.com>
2025-07-31 08:38:58 +0200

    Add pace service

---
+++ GameShared/Services/Interfaces/IPaceService.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IPaceService.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameShared.Services.Interfaces
+{
+    public interface IPaceService
+    {
+        double ToPace(double value, Pace pace);
+    }
+}
---
+++ GameShared/Services/PaceService.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="PaceService.cs" company="BTRemake">
+// Copyright (c) BTRemake. All rights reserved.
+// </copyright>
+
+namespace GameShared.Services
+{
+    using System;
+    using GameShared.Services.Interfaces;
+
+    public enum Pace
+    {
+        None = 0,
+        Minutes,
+        Hours,
+        Days,
+        Months,
+    }
+
+    internal class PaceService : IPaceService
+    {
+        public const double MinutesToPace = 1;
+        public const double HoursToPace = MinutesToPace / 60;
+        public const double DaysToPace = HoursToPace / 24;
+        public const double MonthToPace = DaysToPace / 30;
+
+        public double ToPace(double value, Pace pace)
+        {
+            switch (pace)
+            {
+                case Pace.Minutes:
+                    return MinutesToPace * value;
+                case Pace.Hours:
+                    return HoursToPace * value;
+                case Pace.Days:
+                    return DaysToPace * value;
+                case Pace.Months:
+                    return MonthToPace * value;
+
+                default:
+                    throw new NotSupportedException($"{pace} nor supported.");
+            }
+        }
+    }
+}
